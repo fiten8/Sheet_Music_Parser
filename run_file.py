bc2a7aec0b5eb6763f4564f2f0ee87e4ca2f8fbd
fiten8 <szymanskin@gmail.com>
2025-12-05 16:45:09 -0500

    create run.py
    finds directory, opens each file and analyzes its contents to prepare for splitting

---
+++ run_file.py
@@ -0,0 +1,58 @@
+import os, pathlib, PyPDF2 as pdf
+from PyPDF2 import PdfReader, PdfWriter
+from pathlib import Path
+
+new_dir_path = "C:\\Users\\Owner\\Downloads\\charts\\"
+os.chdir(new_dir_path)
+
+writer = PdfWriter
+
+parts = ["Alto_1","Alto_2", "Tenor_1", "Tenor_2", "Baritone", 
+         "Trumpet_1", "Trumpet_2", "Trumpet_3", "Trumpet_4",
+         "Trombone_1", "Trombone_2", "Trombone_3", "Trombone_4", 
+         "Rhythm","Vocal"]
+
+pages1 = [1,1,1,1,1,
+          1,1,1,1,
+          1,1,1,1,
+          4,0]
+
+pages2 = [2,2,2,2,2,
+          2,2,2,2,
+          2,2,2,2,
+          8,0]
+
+pages3 = [3,3,3,3,3,
+          3,3,3,3,
+          3,3,3,3,
+          12,0]
+
+pdf_files = Path(new_dir_path).glob("*.pdf")
+for pdf_file in pdf_files:
+    reader = PdfReader(pdf_file)
+    file = open(pdf_file, "rb")
+    len_of_pdf = len(reader.pages)
+    if len_of_pdf <= 30:
+        temp_list = list(pages1)
+    elif len_of_pdf > 30 and len_of_pdf <= 42:
+        temp_list = list(pages2)
+    elif len_of_pdf < 60:
+        temp_list = list(pages3)
+    else:
+        print("File ", pdf_file.name, " has too many pages.")
+        continue
+    #print(pdf_file.name," has ", len_of_pdf, "pages. Using page", temp_list[0])
+          
+    if pdf_file.name.__contains__("_v"):
+        vocal_pages = pdf_file.name.split("_v")[1]
+        #print("Vocal pages detected:", vocal_pages)
+        vocal_pages = vocal_pages.split(".pdf")[0]
+        #print("Vocal pages part 2:", vocal_pages)
+        temp_list[14] = vocal_pages
+
+
+
+
+#    for page_num in range(len_of_pdf):
+#        pass
+#    print(reader.pages[0].extract_text())
