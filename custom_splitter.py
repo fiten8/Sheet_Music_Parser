c3b44effe1491190c59262c5a5ce98a58e3a2068
fiten8 <szymanskin@gmail.com>
2025-12-16 16:37:20 -0500

    expand rhythm section

--- custom_splitter.py
+++ custom_splitter.py
@@ -1,45 +1,51 @@
-import os, pathlib, PyPDF2 as pdf
+import os, pathlib
 from PyPDF2 import PdfReader, PdfWriter
-from pathlib import Path
-
-new_dir_path = "C:\\Users\\Owner\\Downloads\\charts\\"
-os.chdir(new_dir_path)
 
 parts = ["Vocal","Alto_1","Alto_2", "Tenor_1", "Tenor_2", "Baritone", 
          "Trumpet_1", "Trumpet_2", "Trumpet_3", "Trumpet_4",
-         "Trombone_1", "Trombone_2", "Trombone_3", "Trombone_4", 
-         "Rhythm"]
+         "Trombone_1", "Trombone_2", "Trombone_3", "Trombone_4",
+         "Guitar","Piano","Bass","Aux_Percussion","Drums"]
 x = 2
-temp_list = [2,x,x,x,x,x,
+temp_list = [0,x,x,x,x,x,
           x,x,x,x,
-          x,x,x,0,
-          20]
+          x,x,x,x,
+          x,2*x,x,0,x]
 
 ### enter file name here ###
-file_name = "Oh Channukah.pdf"
-pdf_files = Path(new_dir_path)+file_name
-truncated_name = pdf_files.stem
-print(truncated_name)
-    
-for j in range(len(temp_list)):
-    if len(temp_list) != len(parts):
-        print("Error: parts list and temp_list length do not match.")
-        break
-    writer = PdfWriter()
-    #print("Creating PDF for ", parts[j], " from ", pdf_file.name)
-    #print("starting on page ", i," adding ", temp_list[j], " pages.")
+truncated_name = "Deck the Halls With All Sorts of Funky"
+
+script_dir = pathlib.Path().absolute()
+output_dir = os.path.join(script_dir, "split")
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+    print("Created output directory: ", output_dir)
+file_name = truncated_name + ".pdf"
+file_path = f"../charts/{file_name}"
+#print(truncated_name)
+
+with open(file_path, "rb") as pdf_file:
+    reader = PdfReader(pdf_file)
+    len_of_pdf = len(reader.pages)
+    #print("Total pages in PDF: ", len_of_pdf)
+
     i = 0
-    for page_num in range(i, i + temp_list[j]):
-        
-        #print("Adding page ", page_num, " for ", parts[j], " from ", pdf_file.name)
-        if temp_list[j] == 0:
-            #print("Skipping ", parts[j], " for ", pdf_file.name,", no vocal pages.")
-            continue
-        if page_num >= len_of_pdf:
-            #print("Reached end of PDF for ", pdf_file.name)
+    for j in range(len(temp_list)):
+        if len(temp_list) != len(parts):
+            print("Error: parts list and temp_list length do not match.")
             break
-        writer.add_page(reader.pages[page_num])
-        i += 1
-    output_file_path = f"split\\{truncated_name}_{parts[j]}.pdf"
-    with open(output_file_path, "wb") as output_pdf:
-        writer.write(output_pdf)
+        writer = PdfWriter()
+        if temp_list[j] == 0:
+                print("Skipping ", parts[j], " for ", pdf_file.name,", no pages.")
+                continue
+        #print("Creating PDF for ", parts[j], " from ", pdf_file.name)
+        #print("starting on page ", i," adding ", temp_list[j], " pages.")
+        for page_num in range(i, i + temp_list[j]):
+            #print("Adding page ", page_num, " for ", parts[j], " from ", truncated_name)
+            if page_num >= len_of_pdf:
+                print("Reached end of PDF for ", pdf_file.name)
+                break
+            writer.add_page(reader.pages[page_num])
+            i += 1
+        output_file_path = f"{output_dir}\\{truncated_name}_{parts[j]}.pdf"
+        with open(output_file_path, "wb") as output_pdf:
+            writer.write(output_pdf)
--- merger.py
+++ merger.py
@@ -1,29 +1,36 @@
-import os, PyPDF2
+import os, pathlib
 from PyPDF2 import PdfReader, PdfWriter
 
 parts = ["Vocal","Alto_1","Alto_2", "Tenor_1", "Tenor_2", "Baritone", 
          "Trumpet_1", "Trumpet_2", "Trumpet_3", "Trumpet_4",
          "Trombone_1", "Trombone_2", "Trombone_3", "Trombone_4", 
-         "Rhythm"]
+         "Guitar","Piano","Bass","Aux_Percussion","Drums"]
         
 # merger
-setlist = ["Oh Channukah","Christmas Time is Here","Feel So Good","Orange Colored Sky",
+setlist = ["Oh Channukah","Christmas Time is Here","Feels So Good","Orange Colored Sky",
            "My Favorite Things","Silent Night (Clark)","Deck the Halls With All Sorts of Funky"]
 performance = "Oakleaf"
 
-new_dir_path = "C:\\Users\\Owner\\Downloads\\charts\\split\\"
-os.chdir(new_dir_path)
-for part in parts:
+script_dir = pathlib.Path().absolute()
+part_index = 0
+while part_index <= len(parts):
     merger = PdfWriter()
     for piece in setlist:
-        file_name = f"{piece}_{part}.pdf"
+        file_name = f"{piece}_{parts[part_index]}.pdf"
         if not os.path.exists(file_name):
-            print("File ", file_name, " does not exist, skipping.")
-            continue
-        #print("Adding ", file_name, " to ", performance, " setlist.")
+            if part_index == 0:
+                print("File ", file_name, " does not exist, skipping.")
+                continue
+            print("File ", file_name, " does not exist, trying previous part to ",parts[part_index],", ",parts[part_index-1])
+            file_name = f"{piece}_{parts[part_index-1]}.pdf"
+            if not os.path.exists(file_name):
+                print("File ", file_name, " does not exist, skipping.")
+                continue
+        print("Adding ", file_name, " to ", performance, " setlist.")
         reader = PdfReader(file_name)
         for page_num in range(len(reader.pages)):
             merger.add_page(reader.pages[page_num])
-    output_file_path = f"{performance}_{part}.pdf"
+    output_file_path = f"{performance}_{parts[part_index]}.pdf"
     with open(output_file_path, "wb") as output_pdf:
         merger.write(output_pdf)
+        part_index += 1
